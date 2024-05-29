6b6cf883389369a0186661f86ab07aef0893664a
Drelmido001 <elmehdidouiri99@gmail.com>
2024-05-29 09:54:46 +0100

    first commit

---
+++ src/main/java/org/example/DataBaseConnection.java
@@ -0,0 +1,17 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DataBaseConnection {
+    public static final String URL="jdbc:mysql://localhost:3306/dbTest";
+    public static final String USER="root";
+    public static final String PASSWORD="";
+    public static Connection getConnection() throws SQLException {
+    try{
+    Class.forName("com.mysql.jdbc.Driver");
+    return DriverManager.getConnection(URL,USER,PASSWORD);
+    }catch (Exception e){
+        throw new SQLException("Driver JDBC introuvable",e);
+    }
+    }
+}
